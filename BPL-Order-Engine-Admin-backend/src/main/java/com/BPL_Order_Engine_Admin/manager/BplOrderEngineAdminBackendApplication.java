fbf1bcb32d09f145c2e6846f568b7596d07a19ed
Sayed Inan <inansayed10@gmail.com>
2026-08-27 14:28:59 +0600

    Initial project setup

---
+++ BPL-Order-Engine-Admin-backend/src/main/java/com/BPL_Order_Engine_Admin/manager/BplOrderEngineAdminBackendApplication.java
@@ -0,0 +1,13 @@
+package com.BPL_Order_Engine_Admin.manager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BplOrderEngineAdminBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BplOrderEngineAdminBackendApplication.class, args);
+	}
+
+}
